7643261f7fa80aaa2ec1779cc442ad1cd027343a
Dat004 <alexanderdat98@gmail.com>
2024-05-03 15:20:15 +0700

    add: add label vip only allow user vip be use

--- src/components/Card/MusicCards/InformationCard.jsx
+++ src/components/Card/MusicCards/InformationCard.jsx
@@ -1,11 +1,14 @@
 import classNames from 'classnames';
 
+import { LabelPremiumIcon } from '../../CustomIcon';
 import ArtistName from '../../ArtistName';
 import TitleCard from '../TitleCard';
 import ImageCard from '../ImageCard';
 
 function InformationCard({ children, className, data = {}, largeSize = false, isShowTimeRelease = false }) {
     const classes = classNames(className);
+
+    const isVip = data?.streamingStatus === 2; // Type VIPS
 
     return (
         <section className="flex items-center justify-start">
@@ -15,9 +18,17 @@ function InformationCard({ children, className, data = {}, largeSize = false, is
                     <ImageCard className={classes} src={data?.thumbnailM || data.thumbnail} />
                 </div>
                 <div className="w-0 flex-grow flex-shrink pr-[10px]">
-                    <TitleCard className={`${largeSize ? '!text-[18px font-bold' : 'text-[14px]'}`}>
-                        {data?.title}
-                    </TitleCard>
+                    <div className="flex items-start">
+                        <TitleCard className={`${largeSize ? '!text-[18px font-bold' : 'text-[14px]'}`}>
+                            {data?.title}
+                        </TitleCard>
+                        {/* Show labels VIPS */}
+                        {isVip && (
+                            <i className="ml-[8px]">
+                                <LabelPremiumIcon width="56px" height="14px" />
+                            </i>
+                        )}
+                    </div>
                     <ArtistName
                         mediumSize={largeSize}
                         smallSize={!largeSize}
